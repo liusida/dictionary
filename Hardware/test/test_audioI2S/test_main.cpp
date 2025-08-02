ed8fbcce34bb6d58466ee2b1542c1e0098755ca1
Sida Liu <learner.sida.liu@gmail.com>
2025-08-02 15:48:13 +0800

    audio

--- Hardware/test/test_audioI2S/test_main.cpp
+++ Hardware/test/test_audioI2S/test_main.cpp
@@ -6,7 +6,7 @@
 #include "FS.h"
 
 #include "pins.h"
-#define Serial Serial0
+// #define Serial Serial0
 
 Audio audio;
 
@@ -34,38 +34,12 @@ void test_main() {
     audio.setPinout(PIN_SOUND_BCLK, PIN_SOUND_LRC, PIN_SOUND_DIN);
     Serial.println("setVolume");
     audio.setVolume(21); // default 0...21
-    // audio.forceMono(true);  // <--- ADD THIS LINE
-//  or alternative
-//  audio.setVolumeSteps(64); // max 255
-//  audio.setVolume(63);
-//
+    audio.forceMono(true);  // <--- ADD THIS LINE
+
 //  *** radio streams ***
     Serial.println("https://dict.liusida.com/audio/explanation/apple.mp3");
     audio.connecttohost("https://dict.liusida.com/audio/explanation/apple.mp3");
-//  audio.connecttohost("http://mcrscast.mcr.iol.pt/cidadefm");                                         // mp3
-//  audio.connecttohost("http://www.wdr.de/wdrlive/media/einslive.m3u");                                // m3u
-//  audio.connecttohost("https://stream.srg-ssr.ch/rsp/aacp_48.asx");                                   // asx
-//  audio.connecttohost("http://tuner.classical102.com/listen.pls");                                    // pls
-//  audio.connecttohost("http://stream.radioparadise.com/flac");                                        // flac
-//  audio.connecttohost("http://stream.sing-sing-bis.org:8000/singsingFlac");                           // flac (ogg)
-//  audio.connecttohost("http://s1.knixx.fm:5347/dein_webradio_vbr.opus");                              // opus (ogg)
-//  audio.connecttohost("http://stream2.dancewave.online:8080/dance.ogg");                              // vorbis (ogg)
-//  audio.connecttohost("http://26373.live.streamtheworld.com:3690/XHQQ_FMAAC/HLSTS/playlist.m3u8");    // HLS
-//  audio.connecttohost("http://eldoradolive02.akamaized.net/hls/live/2043453/eldorado/master.m3u8");   // HLS (ts)
-//  *** web files ***
-//  audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/Pink-Panther.wav");        // wav
-//  audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/Santiano-Wellerman.flac"); // flac
-//  audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/Olsen-Banden.mp3");        // mp3
-//  audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/Miss-Marple.m4a");         // m4a (aac)
-//  audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/Collide.ogg");             // vorbis
-//  audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/sample.opus");             // opus
-//  *** local files ***
-//  audio.connecttoFS(SD, "/test.wav");     // SD
-//  audio.connecttoFS(SD_MMC, "/test.wav"); // SD_MMC
-//  audio.connecttoFS(SPIFFS, "/test.wav"); // SPIFFS
 
-//  audio.connecttospeech("Wenn die Hunde schlafen, kann der Wolf gut Schafe stehlen.", "de"); // Google TTS
-    // TEST_ASSERT_TRUE(true);
 }
 
 void setup() {
@@ -80,6 +54,11 @@ void setup() {
 
 void loop(){
     audio.loop();
+    if (!audio.isRunning()) {
+        Serial.println("play again");
+        audio.connecttohost("https://dict.liusida.com/audio/explanation/apple.mp3");
+
+    }
     vTaskDelay(1);
 }
 
