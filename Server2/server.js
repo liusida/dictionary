a850d212f1ecc23488d2048fc2760679cee1ad11
Sida Liu <learner.sida.liu@gmail.com>
2025-08-06 00:12:12 +0800

    set public as the static folder

--- Server2/server.js
+++ Server2/server.js
@@ -197,6 +197,7 @@ app.use(
   })
 );
 app.use(express.json({ limit: "1mb" }));
+app.use(express.static('public'));
 
 // --- Serve cached audio (waits if not ready) ---
 app.get("/api/audio", async (req, res) => {
